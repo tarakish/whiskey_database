ec2c026631c8abda75cbe80155d5f1d2afad524f
tarakish <akr.3838soccer@gmail.com>
2022-02-13 03:03:47 +0900

    Chore: Semantics UIの導入 #19

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
 const { environment } = require('@rails/webpacker')
+// jquerryの読み込み
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery/src/jquery',
+    jQuery: 'jquery/src/jquery'
+  })
+)
 
 module.exports = environment
