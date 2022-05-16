6329156993707114401ee09f2cf1f52e71eeea6c
tarakish <akr.3838soccer@gmail.com>
2022-05-16 16:27:41 +0900

    Appearance: ウイスキー詳細でのフレーバーの動的表示の色の制御 #75

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def label(flavor)
+    group = flavor.group
+    case group
+      when 'woody'
+        'woody'
+      when 'winy'
+        'red'
+      when 'fruity'
+        'orange'
+      when 'floral'
+        'pink'
+      when 'sereal'
+        'yellow'
+      when 'smoky'
+        'teal'
+    end
+  end
 end
