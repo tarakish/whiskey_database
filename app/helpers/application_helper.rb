3a04ee2176223931e0089b3faaaec942bea068c5
tarakish <akr.3838soccer@gmail.com>
2022-05-16 17:04:42 +0900

    Fix: 不要なコードの削除と微修正 #75

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,2 @@
 module ApplicationHelper
-  def label(flavor)
-    group = flavor.group
-    case group
-      when 'woody'
-        'woody'
-      when 'winy'
-        'red'
-      when 'fruity'
-        'orange'
-      when 'floral'
-        'pink'
-      when 'sereal'
-        'yellow'
-      when 'smoky'
-        'teal'
-    end
-  end
 end
