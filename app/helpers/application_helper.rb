77489a5a587dc7a4057265301c81b681ea3fdff2
tarakish <akr.3838soccer@gmail.com>
2022-05-19 22:57:11 +0900

    Feat: トップページとその他のページでのheaderの出し分け実装 #89

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def separate_header
+    if controller_name != 'staticpages'
+      return 'header-main'
+    end
+  end
+
+  def space_head
+    if controller_name != 'staticpages'
+      return 'mt-7'
+    end
+  end
 end
