3f5194a66847bfe7bc5bc74d98c6ec8122ce8749
tarakish <akr.3838soccer@gmail.com>
2022-05-22 00:16:47 +0900

    Refactor: rubocopの修正 #103

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    flash[:alert] = "You are not authorized to perform this action."
+    flash[:alert] = 'You are not authorized to perform this action.'
     redirect_back(fallback_location: whiskeys_path)
   end
 end
