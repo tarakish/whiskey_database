daf721ed9d41acdc517598cd6f079bfca5f88071
tarakish <akr.3838soccer@gmail.com>
2022-05-21 18:19:04 +0900

    Refactor: robocopの修正 #96

--- app/models/whiskey.rb
+++ app/models/whiskey.rb
@@ -52,8 +52,7 @@ class Whiskey < ApplicationRecord
                       message:
                       I18n.t('activerecord.errors.messages.a_whiskey_has_upto_three_flavors') }
 
-  enum mouth_feel: { light: 0, a_little_light: 1, balanced: 2,
-                                         a_little_rich: 3, rich: 4 }
+  enum mouth_feel: { light: 0, a_little_light: 1, balanced: 2, a_little_rich: 3, rich: 4 }
   enum flavor_strength: { delicate: 0, a_little_delicate: 1, normal: 2, a_little_smoky: 3,
                           smoky: 4 }
   enum rarity: { rare: 0, a_little_rare: 1, stable_supplyed: 2 }
