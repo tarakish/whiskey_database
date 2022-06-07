517cbc7963b82fea344b1e9b07d8326f92d807d3
tarakish <akr.3838soccer14@gmail.com>
2022-06-08 00:58:15 +0900

    Feat: 編集モーダルの実装 #141

--- app/controllers/tasting_notes_controller.rb
+++ app/controllers/tasting_notes_controller.rb
@@ -1,24 +1,17 @@
 class TastingNotesController < ApplicationController
   skip_before_action :require_login, only: :index
-  before_action :set_tasting_note, only: %i[edit update destroy]
+  before_action :set_tasting_note, only: %i[update destroy]
 
   def index
     @tasting_notes = TastingNote.all
   end
-
-  def new
-    @whiskey = Whiskey.find(params[:whiskey_id])
-    @tasting_note = current_user.tasting_notes.build
-  end
-
-  def edit; end
 
   def create
     @whiskey = Whiskey.find(params[:whiskey_id])
     @tasting_note =
       current_user.tasting_notes.build(tasting_note_params.merge(whiskey_id: @whiskey.id))
     if @tasting_note.save
-      redirect_to whiskey_path(@whiskey), notice: 'テイスティングノートを投稿しました'
+      redirect_to whiskey_url(@whiskey), notice: 'テイスティングノートを投稿しました'
     else
       render :new
     end
@@ -26,9 +19,10 @@ class TastingNotesController < ApplicationController
 
   def update
     if @tasting_note.update(tasting_note_params)
-      redirect_to tasting_note_url(@tasting_note), notice: 'テイスティングノートを更新しました'
+      redirect_to whiskey_url(@tasting_note.whiskey_id), notice: 'テイスティングノートを更新しました'
     else
-      render :edit
+      @whiskey = @tasting_note.whiskey
+      render 'whiskeys/show'
     end
   end
 
