40b01b512b502dfa48b90e6557e460e002ef163c
tarakish <akr.3838soccer14@gmail.com>
2022-06-08 07:12:41 +0900

    Feat: 投稿モーダルのデザイン完了 #141

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,12 @@
+const { popper } = require('@popperjs/core')
 const { environment } = require('@rails/webpacker')
 // jquerryの読み込み
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    jQuery: 'jquery/src/jquery',
+    Popper: 'popper.js'
   })
 )
 
