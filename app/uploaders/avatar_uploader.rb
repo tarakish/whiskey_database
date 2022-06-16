87fcf2ca2e400a7a78703dedb2ee8a222ee63516
tarakish <akr.3838soccer14@gmail.com>
2022-06-16 22:04:17 +0900

    Feat: ユーザー編集での画像表示 #147

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -35,9 +35,14 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_allowlist
+    %w(jpg jpeg png webp)
+  end
+
+  def content_type_allowlist
+    /image\//
+  end
+
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
