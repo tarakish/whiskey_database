2560ad86b6995a6fe91ab33880194670cc693aa9
tarakish <akr.3838soccer14@gmail.com>
2022-06-29 12:02:38 +0900

    Feat: エラーページのハンドリング完了 #168

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include Pundit::Authorization
   include Pagy::Backend
+  rescue_from Pundit::NotAuthorizedError, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+  rescue_from StandardError, with: :render_500
   add_flash_types :success, :info, :warning, :danger
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   before_action :set_search_instance
   before_action :require_login
   before_action :ensure_domain, if: :production?
+  before_action :set_random_whiskey, only: %i[render_404 render_500]
 
   def ensure_domain
     return unless /\.herokuapp.com/.match?(request.host)
@@ -19,12 +23,15 @@ class ApplicationController < ActionController::Base
     Rails.env.production?
   end
 
-  private
-
-  def user_not_authorized
-    flash[:alert] = 'ページが見つかりません'
-    redirect_back(fallback_location: root_path)
+  def render_404
+    render template: 'errors/404', status: 404
   end
+
+  def render_500
+    render template: 'errors/500', status: 500
+  end
+
+  private
 
   def set_search_instance
     @q = Whiskey.ransack(params[:q])
@@ -32,5 +39,9 @@ class ApplicationController < ActionController::Base
 
   def not_authenticated
     redirect_to login_url, danger: 'この機能の利用にはログインが必要です'
+  end
+
+  def set_random_whiskey
+    @whiskey = Whiskey.where( 'id >= ?', rand(Whiskey.first.id..Whiskey.last.id) ).first
   end
 end
